dd2f6f9dbd23cbb1ede8fd5a743487ac75370d17
Erum azeemi qaimkhani <160758395+erumQaimkhani@users.noreply.github.com>
2025-02-14 23:42:05 +0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,3 @@ import { clsx } from "clsx"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-// lib/utils.ts
-// export function cn(...inputs: (string | undefined | null | false)[]): string {
-//   return inputs.filter(Boolean).join(" ");
-// }
